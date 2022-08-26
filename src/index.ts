ddaf260a025cdabb08dc5ece3c557ce0a82516c6
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-08-26 16:29:40 -0400

    agrego ruta diaries

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import diaryRouter from './routes/diaries'
 const app = express()
 app.use(express.json())
 
@@ -8,6 +9,9 @@ app.get('/ping', (_req, res) => {
     console.log('someone pinged here!! ' + new Date().toLocaleDateString())
     res.send('pong')
 })
+
+app.use('/api/diaries', diaryRouter)
+
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
